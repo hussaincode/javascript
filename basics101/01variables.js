20aa7f3d4a1d5ebb9de0c50df42ff3314d0783b9
Mohammad Hussain <hussainsaddan786@gmail.com>
2023-10-16 15:50:07 +0000

    data type

---
+++ basics101/01variables.js
@@ -0,0 +1,14 @@
+const accountId = 122345;
+let accountEmail = "hussaincode@gmail.com";
+var accountPass ="12345";
+accountCity = "Samastipur";
+let accountState;
+
+
+// accountId=2;
+console.log(accountId);
+
+accountEmail="hussain@gmail.com";
+accountPass="54321";
+accountCity="Bangaluru";
+console.table([accountEmail,accountPass,accountCity,accountState])
---
+++ basics101/02datatypes.js
@@ -0,0 +1,21 @@
+"use strict" ; //traet all js code as newer version
+
+// alert(1+1);  //we are using nodejs not browser that's why it won't work here.
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
